9012181c4db433e14da93ea70f08074bf649a4d3
amaan ansari <ansari.amaan@outlook.com>
2024-12-29 13:06:15 +0530

    changes

--- data_drive/urls.py
+++ data_drive/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     path('folder/<int:folder_id>/', views.folder_detail, name='folder_detail'),
     path('file/<int:file_id>/', views.file_detail, name='file_detail'),
     path('folder/create/', views.folder_create, name='folder_create'),
-    path('folder/create/<int:parent_folder_id>/', views.folder_create, name='folder_create'),  # Added this line
+    path('folder/create/<int:parent_folder_id>/', views.folder_create, name='folder_create'),
     path('file/create/<int:folder_id>', views.file_create, name='file_create'),
     path('folder/<int:folder_id>/update/', views.folder_update, name='folder_update'),
     path('file/<int:file_id>/update/', views.file_update, name='file_update'),
     path('folder/<int:folder_id>/delete/', views.folder_delete, name='folder_delete'),
     path('file/<int:file_id>/delete/', views.file_delete, name='file_delete'),
-    path('folder/<int:parent_folder_id>/', views.folder_list, name='folder_list'),  # Added this line
+    path('folder/<int:parent_folder_id>/', views.folder_list, name='folder_list'),
 
 ]
